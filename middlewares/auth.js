5b8b6dc865a9039bc53d57915cd01c29cb5f9f08
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-30 03:24:21 +0300

    added custom error handling

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const CustomError = require('../errors/customError');
 
 const { JWT_SECRET, NODE_ENV } = process.env;
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-secret');
   } catch (err) {
-    // todo
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new CustomError('Необходима авторизация', '401'));
   }
   req.user = payload;
   next();
