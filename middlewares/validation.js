a5eea7228ebb6733daf9b3d5e3c4340339938587
Nikita Inkin <nikitainkin@yahoo.com>
2020-04-30 03:25:24 +0300

    minor fixes to validation

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -24,8 +24,8 @@ const signinValidator = celebrate({
 });
 const signupValidator = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
     name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(8).max(30),
   }),
 });
